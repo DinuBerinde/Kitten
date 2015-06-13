02e8a7ec110783bd6b3d974caf3b8e6d41487f39
Dinu Berinde <dinu2193@gmail.com>
2015-06-13 16:35:48 +0200

    update

--- src/javaBytecodeGenerator/TestClassGenerator.java
+++ src/javaBytecodeGenerator/TestClassGenerator.java
@@ -1,16 +1,11 @@
 package javaBytecodeGenerator;
 
 import java.util.Set;
-
 import org.apache.bcel.Constants;
 import org.apache.bcel.classfile.Field;
 import org.apache.bcel.generic.ALOAD;
 import org.apache.bcel.generic.ASTORE;
 import org.apache.bcel.generic.ConstantPoolGen;
-import org.apache.bcel.generic.DDIV;
-import org.apache.bcel.generic.DLOAD;
-import org.apache.bcel.generic.DSTORE;
-import org.apache.bcel.generic.DSUB;
 import org.apache.bcel.generic.FieldGen;
 import org.apache.bcel.generic.GETSTATIC;
 import org.apache.bcel.generic.IADD;
@@ -18,6 +13,9 @@ import org.apache.bcel.generic.ISUB;
 import org.apache.bcel.generic.InstructionConstants;
 import org.apache.bcel.generic.InstructionFactory;
 import org.apache.bcel.generic.InstructionList;
+import org.apache.bcel.generic.LLOAD;
+import org.apache.bcel.generic.LSTORE;
+import org.apache.bcel.generic.LSUB;
 import org.apache.bcel.generic.LocalVariableGen;
 import org.apache.bcel.generic.MethodGen;
 import org.apache.bcel.generic.PUSH;
@@ -143,29 +141,11 @@ public class TestClassGenerator extends JavaClassGenerator {
 		il.append(getFactory().createPutStatic(this.getClassName(), "totalTests", Type.INT));
 
 
-		// we create the local variable totalTime
-		LocalVariableGen totalTime = methodGen.addLocalVariable("totalTime", Type.DOUBLE, null, null);
-		int indexTotalTime = totalTime.getIndex();
-
-		// and then we call System.currentTimeMillis 
-		il.append(getFactory().createInvoke("java.lang.System", "currentTimeMillis", Type.LONG,
-				Type.NO_ARGS, Constants.INVOKESTATIC));
-
-		il.append(getFactory().createCast(Type.LONG, Type.DOUBLE));
-		il.append(getFactory().createConstant(1.00));
-		il.append(new DDIV());
-
-		// and we save the the value into totalTime
-		il.append(new DSTORE(indexTotalTime));
-
-
 		// we build an object that has the type of the class were the test is defined
 		NEW obj = new NEW(this.classType);
-
 		// we translate the obj to JB
 		il.append(obj.generateJavaBytecode(this));
 		il.append(InstructionFactory.DUP);
-
 		// we invoke the constructor to create the obj
 		il.append(getFactory().createInvoke(
 				this.classType.toBCEL().toString(),  // the name of the class
@@ -177,24 +157,23 @@ public class TestClassGenerator extends JavaClassGenerator {
 		// we create the local variable obj
 		LocalVariableGen o = methodGen.addLocalVariable("obj", obj.getType().toBCEL(), null, null);
 		int indexObj = o.getIndex();
-
 		// and we save the created obj into the local variable obj
 		il.append(new ASTORE(indexObj));
 
+
+		// we create the local variable totalTime
+		LocalVariableGen totalTime = methodGen.addLocalVariable("totalTime", Type.LONG, null, null);
+		int indexTotalTime = totalTime.getIndex();
+		// and then we call System.currentTimeMillis 
+		il.append(getFactory().createInvoke("java.lang.System", "currentTimeMillis", Type.LONG, Type.NO_ARGS, Constants.INVOKESTATIC));
+		// and we save the the value into totalTime
+		il.append(new LSTORE(indexTotalTime));
+
+
 		// we create a local variable time
-		LocalVariableGen time = methodGen.addLocalVariable("time", Type.DOUBLE, null, null);
+		LocalVariableGen time = methodGen.addLocalVariable("time", Type.LONG, null, null);
 		int indexTime = time.getIndex();
 
-		// and then we call System.currentTimeMillis 
-		il.append(getFactory().createInvoke("java.lang.System", "currentTimeMillis", Type.LONG,
-				Type.NO_ARGS, Constants.INVOKESTATIC));
-
-		il.append(getFactory().createCast(Type.LONG, Type.DOUBLE));
-		il.append(getFactory().createConstant(1.00));
-		il.append(new DDIV());
-
-		// and then we save the result into time
-		il.append(new DSTORE(indexTime));
 
 		// we call all tests with obj as argument
 		for(TestSignature test: this.classType.getTests().values()){
@@ -202,6 +181,13 @@ public class TestClassGenerator extends JavaClassGenerator {
 
 			il.append(new PUSH(this.getConstantPool(), "\t -" + test.getName() + ": passed ["));
 			il.append(getFactory().createPutStatic(getClassName(), "flag", Type.STRING));
+
+
+			// we call currentTimeMillis
+			il.append(getFactory().createInvoke("java.lang.System", "currentTimeMillis", Type.LONG, Type.NO_ARGS, Constants.INVOKESTATIC));
+			// and then we save the result into time
+			il.append(new LSTORE(indexTime));
+
 
 			il.append(getFactory().createInvoke(this.classType.toBCEL().toString() + "Test",
 					test.getName(),
@@ -211,23 +197,26 @@ public class TestClassGenerator extends JavaClassGenerator {
 							Constants.INVOKESTATIC));
 
 
-			// we save the time that passed for this test
+			// field to save the time of this test
 			FieldGen fieldTime = new FieldGen(Constants.ACC_PRIVATE | Constants.ACC_STATIC, Type.DOUBLE, test.getName() + "Time", this.getConstantPool());
 			this.addField(fieldTime.getField());
+
+			// we calculate the time for this test
 			il.append(getFactory().createInvoke("java.lang.System", "currentTimeMillis", Type.LONG,	Type.NO_ARGS, Constants.INVOKESTATIC));
+			il.append(new LLOAD(indexTime));
+			il.append(new LSUB());			
 			il.append(getFactory().createCast(Type.LONG, Type.DOUBLE));
-			il.append(getFactory().createConstant(1.00));
-			il.append(new DDIV());
-			il.append(new DLOAD(indexTime));
-			il.append(new DSUB());
+			// we save the result
 			il.append(getFactory().createPutStatic(getClassName(), test.getName() + "Time", Type.DOUBLE));
 
+			
 			// we increment the number of totalTests
 			il.append(getFactory().createGetStatic(getClassName(), "totalTests", Type.INT));
 			il.append(new PUSH(this.getConstantPool(), 1));
 			il.append(new IADD());
 			il.append(getFactory().createPutStatic(getClassName(), "totalTests", Type.INT));
 
+			
 			// we create a field with the name of the test 
 			FieldGen fieldGen1 = new FieldGen(Constants.ACC_PRIVATE | Constants.ACC_STATIC, Type.STRING, test.getName(), this.getConstantPool());
 			this.addField(fieldGen1.getField());
@@ -238,6 +227,14 @@ public class TestClassGenerator extends JavaClassGenerator {
 			il.append(getFactory().createPutStatic(getClassName(), test.getName(), Type.STRING));
 
 		}
+
+
+		// we calculate the total time of the tests
+		il.append(getFactory().createInvoke("java.lang.System", "currentTimeMillis", Type.LONG,	Type.NO_ARGS, Constants.INVOKESTATIC));
+		il.append(new LLOAD(indexTotalTime));
+		il.append(new LSUB());		
+		// we save the result
+		il.append(new LSTORE(indexTotalTime));
 
 
 		// and all fixtures with obj as argument
@@ -301,7 +298,7 @@ public class TestClassGenerator extends JavaClassGenerator {
 					il.append(getFactory().createGetStatic(getClassName(), testName, Type.STRING));	
 					il.append(getFactory().createInvoke("java.lang.StringBuffer", "append",	Type.STRINGBUFFER, new Type[] { Type.STRING}, Constants.INVOKEVIRTUAL));
 					il.append(getFactory().createGetStatic(getClassName(), testName + "Time", Type.DOUBLE));
-					il.append(getFactory().createInvoke("java.lang.StringBuffer", "append",	Type.STRINGBUFFER, new Type[] { Type.DOUBLE }, Constants.INVOKEVIRTUAL));
+					il.append(getFactory().createInvoke("java.lang.StringBuffer", "append",	Type.STRINGBUFFER, new Type[] { Type.DOUBLE}, Constants.INVOKEVIRTUAL));
 					il.append(new PUSH(this.getConstantPool(), "ms]"));
 					il.append(getFactory().createInvoke("java.lang.StringBuffer", "append", Type.STRINGBUFFER, new Type[] { Type.STRING }, Constants.INVOKEVIRTUAL));
 					il.append(new PUSH(this.getConstantPool(), "\n"));
@@ -323,22 +320,15 @@ public class TestClassGenerator extends JavaClassGenerator {
 		il.append(new PUSH(this.getConstantPool(), " tests passed, "));
 		il.append(getFactory().createInvoke("java.lang.StringBuffer", "append",	Type.STRINGBUFFER, new Type[] { Type.STRING }, Constants.INVOKEVIRTUAL));
 
-
 		// we push on the stack the number of failed tests
 		il.append(getFactory().createGetStatic(getClassName(), "failedTest", Type.INT));
 		il.append(getFactory().createInvoke("java.lang.StringBuffer", "append",	Type.STRINGBUFFER, new Type[] { Type.INT }, Constants.INVOKEVIRTUAL));
 		il.append(new PUSH(this.getConstantPool(), " failed ["));
 		il.append(getFactory().createInvoke("java.lang.StringBuffer", "append",	Type.STRINGBUFFER, new Type[] { Type.STRING }, Constants.INVOKEVIRTUAL));
 
-		// we calculate the total time
-		il.append(getFactory().createInvoke("java.lang.System", "currentTimeMillis", Type.LONG,	Type.NO_ARGS, Constants.INVOKESTATIC));
-
-		il.append(getFactory().createCast(Type.LONG, Type.DOUBLE));
-		il.append(getFactory().createConstant(1.00));
-		il.append(new DDIV());
-		il.append(new DLOAD(indexTotalTime));
-		il.append(new DSUB());
-
+		// we push on the stack the total time of the tests
+		il.append(new LLOAD(indexTotalTime));
+		il.append(getFactory().createCast(Type.LONG, Type.DOUBLE));	
 		il.append(getFactory().createInvoke("java.lang.StringBuffer", "append",	Type.STRINGBUFFER, new Type[] { Type.DOUBLE}, Constants.INVOKEVIRTUAL));
 		il.append(new PUSH(this.getConstantPool(), "ms]"));
 		il.append(getFactory().createInvoke("java.lang.StringBuffer", "append", Type.STRINGBUFFER, new Type[] { Type.STRING }, Constants.INVOKEVIRTUAL));
